1bd72180c5c07a3832fe6236bcd0c862f79a8bdf
GZMaster <danigamester@gmail.com>
2022-06-10 18:38:52 +0100

    cleaning up details

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -26,13 +26,12 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { robots, searchfield } = this.state
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading!</h1>
-        } else {
-            return (
+        return !robots.length ? <h1>Loading!</h1> : 
+            (
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
@@ -41,7 +40,6 @@ class App extends Component {
                     </Scroll>
                 </div>
             )
-        }
     }
 }
 

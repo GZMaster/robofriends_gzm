c5f7202717f47610052b364b603b7140f0eb2dd5
GZMaster <danigamester@gmail.com>
2022-06-10 18:31:21 +0100

    organising the project files

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import CardList from './CardList'
-import SearchBox from './SearchBox'
-import Scroll from './Scroll'
+import CardList from '../Components/CardList'
+import SearchBox from '../Components/SearchBox'
+import Scroll from '../Components/Scroll'
 import './App.css'
 
 
@@ -26,10 +26,10 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0) { 
+        if (this.state.robots.length === 0) {
             return <h1>Loading!</h1>
         } else {
             return (
